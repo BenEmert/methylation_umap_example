2e367263cb5206d2123c7fdbbbdc4b958b80a60e
BenEmert <BenEmert@users.noreply.github.com>
2025-12-18 14:41:45 -0800

    Updating make_plots.R

--- code/make_plots.R
+++ code/make_plots.R
@@ -179,42 +179,42 @@ meta <- meta[common_ids, ]
 # ------------------------------------------------------------------------------
 message("   Plotting Legend...")
 
-# legend_key <- meta %>%
-#   select(Dx, Color_dx) %>%
-#   distinct() %>%
-#   arrange(Dx)
+legend_key <- meta %>%
+  select(Dx, Color_dx) %>%
+  distinct() %>%
+  arrange(Dx)
 
-# dummy_plot <- ggplot(legend_key, aes(x = 1, y = 1, color = Color_dx)) +
-#   geom_point(alpha = 0) + 
-#   scale_color_identity(
-#     breaks = legend_key$Color_dx,     
-#     labels = legend_key$Dx,            
-#     guide = guide_legend(
-#       ncol = 2,                        
-#       override.aes = list(
-#         alpha = 1,                     
-#         size = 5,                      
-#         shape = 15                     
-#       )
-#     )
-#   ) +
-#   theme_void() +
-#   theme(
-#     legend.position = "right",
-#     legend.text = element_text(size = 10),
-#     legend.key = element_blank()      
-#   )
+dummy_plot <- ggplot(legend_key, aes(x = 1, y = 1, color = Color_dx)) +
+  geom_point(alpha = 0) + 
+  scale_color_identity(
+    breaks = legend_key$Color_dx,     
+    labels = legend_key$Dx,            
+    guide = guide_legend(
+      ncol = 2,                        
+      override.aes = list(
+        alpha = 1,                     
+        size = 5,                      
+        shape = 15                     
+      )
+    )
+  ) +
+  theme_void() +
+  theme(
+    legend.position = "right",
+    legend.text = element_text(size = 10),
+    legend.key = element_blank()      
+  )
 
-# legend_only <- get_legend(dummy_plot)
-# n_rows <- ceiling(nrow(legend_key) / 2)
-# calc_height <- max(4, n_rows * 0.25)
-# print(dummy_plot)
-# ggsave(
-#   file.path(plot_dir, "dx_legend.pdf"), 
-#   plot = ggdraw(legend_only), 
-#   width = 6,      
-#   height = calc_height
-# )
+legend_only <- get_legend(dummy_plot)
+n_rows <- ceiling(nrow(legend_key) / 2)
+calc_height <- max(4, n_rows * 0.25)
+print(dummy_plot)
+ggsave(
+  file.path(plot_dir, "dx_legend.pdf"), 
+  plot = ggdraw(legend_only), 
+  width = 6,      
+  height = calc_height
+)
 
 # Random State Comparison
 # ------------------------------------------------------------------------------
@@ -240,88 +240,88 @@ ggsave(file.path(plot_dir, "umap_GPL13534_M_random_state_comparison_R.pdf"), rng
 combined <- bind_rows(emb_list)
 
 dat0 <- combined %>% filter(random_state == "random state = 0")
-p_zoom0 <- plot_umap_gg(dat0, xlim=c(0.5, 7), ylim=c(-3.0, 3.0))
+p_zoom0 <- plot_umap_gg(dat0, xlim=c(0.8, 7.1), ylim=c(-3.0, 3.5))
 ggsave(file.path(plot_dir, "umap_GPL13534_M_rng0_zoom1.pdf"), p_zoom0, width=7, height=7)
 
-p_box0 <- plot_umap_gg(dat0, rect_coords=list(xmin=0.0, xmax=7.5, ymin=-3.5, ymax=3.5))
+p_box0 <- plot_umap_gg(dat0, rect_coords=list(xmin=0.3, xmax=7.5, ymin=-3.5, ymax=4.0))
 ggsave(file.path(plot_dir, "umap_GPL13534_M_rng0_box1.pdf"), p_box0, width=7, height=7)
 
 dat1 <- combined %>% filter(random_state == "random state = 1")
-p_zoom1 <- plot_umap_gg(dat1, xlim=c(1, 7.2), ylim=c(-3.5, 2.5))
-ggsave(file.path(plot_dir, "umap_GPL13534_M_rng1_zoom1.pdf"), p_zoom1, width=7, height=7)
+p_zoom1 <- plot_umap_gg(dat1, xlim=c(1.4, 7.2), ylim=c(-3.5, 3.1))
+ggsave(file.path(plot_dir, "umap_GPL13534_M_rng1_zoom1_3.pdf"), p_zoom1, width=7, height=7)
 
-p_box1 <- plot_umap_gg(dat1, rect_coords=list(xmin=0.5, xmax=7.7, ymin=-4.0, ymax=3.0))
+p_box1 <- plot_umap_gg(dat1, rect_coords=list(xmin=0.9, xmax=7.7, ymin=-4.0, ymax=3.5))
 ggsave(file.path(plot_dir, "umap_GPL13534_M_rng1_box1.pdf"), p_box1, width=7, height=7)
 
 # Neighbors Comparison
 # ------------------------------------------------------------------------------
-# message("   [PLOTTING] UMAP Neighbors Comparison...")
-# plot_list <- list()
+message("   [PLOTTING] UMAP Neighbors Comparison...")
+plot_list <- list()
 
-# for (nn in c(5, 15, 50)) {
-#   tmp <- run_umap(meth_df, common_ids, n_neighbors=nn, seed=0)
+for (nn in c(5, 15, 50)) {
+  tmp <- run_umap(meth_df, common_ids, n_neighbors=nn, seed=0)
 
-#   tmp$n_neighbors <- paste0("n_neighbors = ", nn)
+  tmp$n_neighbors <- paste0("n_neighbors = ", nn)
 
-#   tmp <- tmp %>% rownames_to_column("ID")
+  tmp <- tmp %>% rownames_to_column("ID")
 
-#   tmp <- left_join(tmp, meta, by="ID")
+  tmp <- left_join(tmp, meta, by="ID")
 
-#   p <- plot_umap_gg(tmp, title_text = paste0("n_neighbors = ", nn))
+  p <- plot_umap_gg(tmp, title_text = paste0("n_neighbors = ", nn))
 
-#   plot_list[[length(plot_list)+1]] <- p
-# }
+  plot_list[[length(plot_list)+1]] <- p
+}
 
-# nn_plots <- wrap_plots(plot_list, nrow = 1)
-# ggsave(file.path(plot_dir, "umap_GPL13534_M_n_neighbors_comparison.pdf"), nn_plots, width=21, height=7, dpi=300)
+nn_plots <- wrap_plots(plot_list, nrow = 1)
+ggsave(file.path(plot_dir, "umap_GPL13534_M_n_neighbors_comparison.pdf"), nn_plots, width=21, height=7, dpi=300)
 
-# # 5. Perplexity Comparison (tSNE)
-# # ------------------------------------------------------------------------------
-# message("   [PLOTTING] tSNE Perplexity Comparison...")
-# plot_list <- list()
+# 5. Perplexity Comparison (tSNE)
+# ------------------------------------------------------------------------------
+message("   [PLOTTING] tSNE Perplexity Comparison...")
+plot_list <- list()
 
-# for (perp in c(5, 15, 50)) {
-#   tmp <- run_tsne(meth_df, common_ids, perplexity=perp, seed=0)
+for (perp in c(5, 15, 50)) {
+  tmp <- run_tsne(meth_df, common_ids, perplexity=perp, seed=0)
 
-#   tmp$perplexity <- paste0("perplexity = ", perp)
+  tmp$perplexity <- paste0("perplexity = ", perp)
 
-#   tmp <- tmp %>% rownames_to_column("ID")
+  tmp <- tmp %>% rownames_to_column("ID")
 
-#   tmp <- left_join(tmp, meta, by="ID")
+  tmp <- left_join(tmp, meta, by="ID")
 
-#   p <- plot_umap_gg(tmp, x_col="tSNE1", y_col="tSNE2", title_text = paste0("perplexity = ", perp))
+  p <- plot_umap_gg(tmp, x_col="tSNE1", y_col="tSNE2", title_text = paste0("perplexity = ", perp))
 
-#   plot_list[[length(plot_list)+1]] <- p
-# }
+  plot_list[[length(plot_list)+1]] <- p
+}
 
-# perp_plots <- wrap_plots(plot_list, nrow = 1)
-# ggsave(file.path(plot_dir, "tsne_GPL13534_M_perplexity_comparison.pdf"), perp_plots, width=21, height=7, dpi=300)
-# # Diagnosis Subsets
-# # ------------------------------------------------------------------------------
-# message("   [PLOTTING] Diagnosis Subsets...")
+perp_plots <- wrap_plots(plot_list, nrow = 1)
+ggsave(file.path(plot_dir, "tsne_GPL13534_M_perplexity_comparison.pdf"), perp_plots, width=21, height=7, dpi=300)
+# Diagnosis Subsets
+# ------------------------------------------------------------------------------
+message("   [PLOTTING] Diagnosis Subsets...")
 
-# plot_list <- list()
+plot_list <- list()
 
-# for (c in unique(meta$WHO_differentiation)) {
-#   sub_meta <- meta %>% filter(WHO_differentiation == c | Dx %in% c("CTRL1", "CTRL3"))
-#   sub_ids <- sub_meta$ID
+for (c in unique(meta$WHO_differentiation)) {
+  sub_meta <- meta %>% filter(WHO_differentiation == c | Dx %in% c("CTRL1", "CTRL3"))
+  sub_ids <- sub_meta$ID
 
-#   if (length(sub_ids) > 15){
-#     tmp <- run_umap(meth_df, sub_ids, n_pcs=25, seed=0)
+  if (length(sub_ids) > 15){
+    tmp <- run_umap(meth_df, sub_ids, n_pcs=25, seed=0)
 
-#     tmp$WHO_differentiation <- c
+    tmp$WHO_differentiation <- c
 
-#     tmp <- tmp %>% rownames_to_column("ID")
+    tmp <- tmp %>% rownames_to_column("ID")
 
-#     tmp <- left_join(tmp, meta, by="ID")
+    tmp <- left_join(tmp, meta, by="ID")
 
-#     p <- plot_umap_gg(tmp, title_text = c, show_legend=TRUE)
-#     new_name <- gsub(" ", "_", c) %>% gsub("/", "_", .)
-#     ggsave(file.path(plot_dir, sprintf("umap_GPL13534_M_%s_noBlood.pdf", new_name)), p, width=7, height=7)
+    p <- plot_umap_gg(tmp, title_text = c, show_legend=TRUE)
+    new_name <- gsub(" ", "_", c) %>% gsub("/", "_", .)
+    ggsave(file.path(plot_dir, sprintf("umap_GPL13534_M_%s_noBlood.pdf", new_name)), p, width=7, height=7)
 
-#     plot_list[[length(plot_list)+1]] <- p
+    plot_list[[length(plot_list)+1]] <- p
 
-#   }
-# }
+  }
+}
 
 message("=== Plots Generated in /plots ===")
